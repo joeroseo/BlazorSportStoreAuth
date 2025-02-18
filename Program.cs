8e75809b566f9a14fd38f9b1ce77906c5c2da112
Joe <joerosejax@gmail.com>
2025-02-17 19:39:13 -0500

    New

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Define the API Base URL
-//var apiBaseUrl = "http://localhost:5000/";
+//var apiBaseUrl = "http://localhost:5000";
 var apiBaseUrl = "https://mysqlmachine.database.windows.net/";
 
 builder.Services.AddSingleton(new ApiSettings { BaseUrl = apiBaseUrl });
